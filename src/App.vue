761a1f070914910683afa094d261582a904d36aa
PedroFariasSosa <pedro.farias.sosa@gmail.com>
2020-10-12 00:19:08 -0500

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/episodes">Episodes</router-link> |
+    <router-link to="/locations">Locations</router-link> |
+    <router-link to="/characters">Charactes</router-link>
+  </div>
+  <router-view/>
+  <hr>
+  <nav>
+    <router-link :to="prevComp">Prev</router-link>
+    <router-link :to="nextComp">Next</router-link>
+  </nav>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+const { ref, provide, computed } = require("vue");
+const { useRoute } = require("vue-router");
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export const info = ref({
+    count: 0,
+    pages: 0,
+    next: 0,
+    prev: 0,
+  });
+
+provide("info", info);
+
+export const route = useRoute();
+
+export const prevComp = computed(() => ({
+  name: route.name,
+  params: { page: info.value.prev || route.params.page },
+
+}));
+export const nextComp = computed(() => ({
+  name: route.name,
+  params: { page: info.value.next || route.params.page },
+
+}));
 </script>
 
 <style>
@@ -21,6 +46,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.data-list {
+  background-color: gray;
+  color: #ffffff;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+  margin-top: 20px;
+}
+li {
+  background-color:rgb(94, 88, 88);
+  padding: 10px 30px;
+}
+nav {
+  background-color: rgb(45, 45, 235);
+  color: #ffffff;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
